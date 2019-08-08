83181b72c9e59859166b5aab1af74d5f80cde030
DespiteDeath <Kogut_nikita@mail.ru>
2019-08-08 20:17:47 +0300

    "add good timestamp"

--- test_net.py
+++ test_net.py
@@ -5,6 +5,7 @@ import time
 import matplotlib.pyplot as plt
 from decimal import Decimal
 import datetime
+import matplotlib.dates as mdates
 
 response = requests.get('https://nodes-testnet.wavesnodes.com/addresses/balance/details/3MtN7bbr8sNsqNA1m9XemxKEdpMAUauosAX')
 output = response.text.split(',')
@@ -53,55 +54,62 @@ def Connect(name, balance):
     mydb = mysql.connector.connect(
       host="localhost",
       user="root",
-      passwd="TYPE_YOUR_PASS",  # напиши пароль для бд
+      passwd="------",  # напиши пароль для бд
       database="mydatabase"
     )
 
     mycursor = mydb.cursor()                                             # создание бд
-    # mycursor.execute("CREATE TABLE test_network (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), balance INT(255), timestamp TIMESTAMP(6))")   # создание таблицы и столбцов
+    # mycursor.execute("CREATE TABLE test_network (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), balance INT(255), "
+    #                  "timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")   # создание таблицы и столбцов
 
-    mycursor.execute(
-        "CREATE TABLE balances (id INT AUTO_INCREMENT PRIMARY KEY, id_balance VARCHAR(255), id_acсount VARCHAR(255), balance_date VARCHAR(255),"
-        "amount VARCHAR(255), id_currency VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE accounts (id INT AUTO_INCREMENT PRIMARY KEY, id_acсount VARCHAR(255), id_trader VARCHAR(255), key_adress VARCHAR(255),"
-        "id_currency VARCHAR(255), api_key1 VARCHAR(255), api_key2 VARCHAR(255), id_marketplace VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE currences (id INT AUTO_INCREMENT PRIMARY KEY, id_currency VARCHAR(255), name VARCHAR(255), ticker VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE trader (id INT AUTO_INCREMENT PRIMARY KEY, id_trader VARCHAR(255), name VARCHAR(255), id_country VARCHAR(255),"
-        "email VARCHAR(255), login VARCHAR(255), pass VARCHAR(255), phone_nbr VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE depo_amounts (id INT AUTO_INCREMENT PRIMARY KEY, id_amount VARCHAR(255), id_acсount VARCHAR(255), id_currency VARCHAR(255),"
-        " sum VARCHAR(255), direction VARCHAR(255), sum_date VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE countries (id INT AUTO_INCREMENT PRIMARY KEY, id_country VARCHAR(255), country_name VARCHAR(255), shortname VARCHAR(255))")
-    mycursor.execute(
-        "CREATE TABLE marketplaces (id INT AUTO_INCREMENT PRIMARY KEY, id_marketplace VARCHAR(255), marketplace VARCHAR(255))")
-
-
-    # sql = "INSERT INTO test_network (name, balance, timestamp) VALUES (%s, %s, %s)"
-    # now = datetime.datetime.now().timestamp()
-    # print(now)
-    # val = (name, int(balance), now)
-    # mycursor.execute(sql, val)
-    # mydb.commit()
-    # print(mycursor.rowcount, "record inserted.")
-    #
-    # mycursor.execute("SELECT * FROM test_network")
-    # myresult = mycursor.fetchall()
-    # for x in myresult:
-    #     print(x)
-    #
-    # Draw_plot(myresult, now)
+    # mycursor.execute(
+    #     "CREATE TABLE balances (id INT AUTO_INCREMENT PRIMARY KEY, id_balance VARCHAR(255), id_acсount VARCHAR(255), balance_date VARCHAR(255),"
+    #     "amount VARCHAR(255), id_currency VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE accounts (id INT AUTO_INCREMENT PRIMARY KEY, id_acсount VARCHAR(255), id_trader VARCHAR(255), key_adress VARCHAR(255),"
+    #     "id_currency VARCHAR(255), api_key1 VARCHAR(255), api_key2 VARCHAR(255), id_marketplace VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE currences (id INT AUTO_INCREMENT PRIMARY KEY, id_currency VARCHAR(255), name VARCHAR(255), ticker VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE trader (id INT AUTO_INCREMENT PRIMARY KEY, id_trader VARCHAR(255), name VARCHAR(255), id_country VARCHAR(255),"
+    #     "email VARCHAR(255), login VARCHAR(255), pass VARCHAR(255), phone_nbr VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE depo_amounts (id INT AUTO_INCREMENT PRIMARY KEY, id_amount VARCHAR(255), id_acсount VARCHAR(255), id_currency VARCHAR(255),"
+    #     " sum VARCHAR(255), direction VARCHAR(255), sum_date VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE countries (id INT AUTO_INCREMENT PRIMARY KEY, id_country VARCHAR(255), country_name VARCHAR(255), shortname VARCHAR(255))")
+    # mycursor.execute(
+    #     "CREATE TABLE marketplaces (id INT AUTO_INCREMENT PRIMARY KEY, id_marketplace VARCHAR(255), marketplace VARCHAR(255))")
 
 
+    sql = "INSERT INTO test_network (name, balance, timestamp) VALUES (%s, %s, %s)"
+    now = datetime.datetime.now()
+    print(now)
+    val = (name, int(balance), now)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
 
-def Draw_plot(_myresult, _now):                       # Вывод графика
-    X = [i for i in range(int(_now))]
-    Y = [_myresult[i][2] for i in X]
-    plt.plot(X, Y)
-    plt.xlabel('Days')
+    mycursor.execute("SELECT * FROM test_network")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+    Draw_plot(myresult, '%d-%m-%y %H:%M')
+
+
+
+def Draw_plot(_myresult, date_format=None):                       # Вывод графика
+    X = [i[3] for i in _myresult]
+    Y = [i[2] for i in _myresult]
+    ax = plt.subplot()
+    ax.plot(X, Y)
+
+    if date_format is not None:
+        xfmt = mdates.DateFormatter(date_format)
+        ax.xaxis.set_major_formatter(xfmt)
+
+    plt.xlabel('Day')
     plt.ylabel('Balance')
     plt.show()
 
